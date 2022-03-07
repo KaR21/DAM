2e160860b7feafa73138557ed429142ebd36e771
Elorza Ortiz <elorza.karmele@uni.lan>
2022-03-07 14:28:02 +0100

    updated

--- DAM_1/DATA-BASE/3.Eval/PHP/HelloWorld.php
+++ DAM_1/DATA-BASE/3.Eval/PHP/HelloWorld.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <style>
+            table, td{
+                border: black 1px solid;
+            }
+        </style>
+    </head>
     <body>
 
         <h1>My first PHP page</h1>
 
         <?php
-            echo "Hello World!";
-        ?>
+            printf("Bi zenbakia formatu desberdinekin: %d: %f: %.2f <br>", 2, 3, 4);
+            for ($x = 0; $x <= 10; $x++) {
+                echo "Hello World! <br>";
+            }
 
+        ?>
+        <br>
+        <table>
+            <?php
+                for ($x = 0; $x <= 10; $x++) {?>
+                    <tr>
+                        <td>
+                            <?php echo $x; ?>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            ?>
+
+        </table>
     </body>
 </html>
