b358b37e8211e1a85f59e767f7e49e5ffd13f3ca
Elorza Ortiz <elorza.karmele@uni.lan>
2022-03-11 09:34:29 +0100

    updated

--- DAM_1/DATA-BASE/3.Eval/PHP/GET_POST_aprobak/formularioaAutoak.php
+++ DAM_1/DATA-BASE/3.Eval/PHP/GET_POST_aprobak/formularioaAutoak.php
@@ -2,25 +2,27 @@
     <body>
         
         <h1>Zure datuak ondo gorde dira!</h1>
-        Your name is: <?php echo $_POST["uname"]; ?><br><br>
+        Your name is: <?php echo $_POST["uname"];?><br><br>
+    
         Your password is: <?php echo $_POST["pswd"]; ?><br><br>
+        Your age is: <?php echo $_POST["age"]; ?><br><br>
         <?php if(isset($_POST["bodywork_type"])){
             $aBT = $_POST["bodywork_type"];
             $X = count($aBT); 
-            echo("You selected $X of body-work type(s):");
+            echo("You selected $X of body-work type(s):");?><br><?php
             for($i=0; $i < $X; $i++)
             {
-                echo($aBT[$i] . " ");
+                echo($aBT[$i] . " ");?><br><?php
             }
         }
         ?><br><br>
         <?php if(isset($_POST["fuel_type"])){
             $aF = $_POST["fuel_type"];
             $X = count($aF); 
-            echo("You selected $X of fuel type(s):");
+            echo("You selected $X of fuel type(s):");?><br><?php
             for($i=0; $i < $X; $i++)
             {
-                echo($aF[$i] . " ");
+                echo($aF[$i] . " ");?><br><?php
             }
         }
         ?><br><br>
@@ -28,10 +30,10 @@
         <?php if(isset($_POST["extra"])){
             $aE = $_POST["extra"];  
             $X = count($aE); 
-            echo("You selected $X of extra(s):");
+            echo("You selected $X of extra(s):");?><br><?php
             for($i=0; $i < $X; $i++)
             {
-                echo($aE[$i] . " ");
+                echo($aE[$i] . " ");?><br><?php
             }
         }
         ?><br><br>
